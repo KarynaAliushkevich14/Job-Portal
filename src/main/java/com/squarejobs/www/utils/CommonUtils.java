12b7a0e37285f7e8e859a6dfd87fec1faf95152a
Karyna Aliushkevich <89156053+KarynaAliushkevich14@users.noreply.github.com>
2023-04-08 23:57:16 +0200

    introduction_date in format "yyyy-MM-dd'T'HH-mm-ss"

--- src/main/java/com/squarejobs/www/utils/CommonUtils.java
+++ src/main/java/com/squarejobs/www/utils/CommonUtils.java
@@ -1,16 +1,10 @@
 package com.squarejobs.www.utils;
 
-import org.apache.commons.lang3.time.FastDateFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.TimeZone;
 
 public class CommonUtils {
     private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
@@ -20,19 +14,7 @@ public class CommonUtils {
         return file.getBytes();
     }
 
-    // date YY-MM-DD  HH-MM-SS
-    public static Date returnCurrentTime () {
-        FastDateFormat formatter = FastDateFormat.getInstance(DATE_FORMAT);
-
-        LocalDateTime currentTime = LocalDateTime.now();
-        String formattedTime = formatter.format(currentTime);
-        try {
-            return formatter.parse(formattedTime);
-        } catch (ParseException e) {
-            return null;
-        }
-    }
-
+    // date YY-MM-DD'T'HH-MM-SS
     public static LocalDateTime getCurrentTime() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
